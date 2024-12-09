9e5479e093404a2a4038549417419757d78b3515
lullek <98174110+lullek@users.noreply.github.com>
2024-12-09 13:36:39 +0100

    Graphic stuff

--- Assets/LightOnOrOff.cs
+++ Assets/LightOnOrOff.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class LightOnOrOff : MonoBehaviour
 {
     Light roomLight;
+    bool effect;
     // Start is called before the first frame update
     void Start()
     {
         roomLight = gameObject.GetComponentInChildren<Light>();
         roomLight.enabled = false;
+        effect = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(TurnOnLight());
+        //StartCoroutine(TurnOnLight());
     }
 
     private IEnumerator TurnOnLight() {
         yield return new WaitForSeconds(3f);
         roomLight.enabled = true;
+    }
+
+    public void LightButton() {
+        if(!roomLight.enabled){
+            roomLight.enabled = true;
+        } else {
+            roomLight.enabled = false;
+        }
+    }
+
+    public void EffectButton() {
+        if(!effect){
+            effect = true;
+        } else {
+            effect = false;
+        }
     }
 
     
